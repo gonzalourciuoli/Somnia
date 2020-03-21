8e8383f676546c34f202b99eacab2850a03526f1
ainagil <ainagiltecno@gmail.com>
2020-03-22 00:43:53 +0100

    class Valuations class Calculator

--- app/src/main/java/com/example/somnia/Valuations.kt
+++ app/src/main/java/com/example/somnia/Valuations.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 
-class valuationsActivity : AppCompatActivity() {
+class Valuations : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,33 +16,33 @@ class valuationsActivity : AppCompatActivity() {
         val home_button = findViewById<ImageView>(R.id.principal_settings) as ImageView
         home_button.setOnClickListener {
             home_button.setColorFilter(Color.RED)
-            val intent = Intent(this@valuationsActivity, Home::class.java)
+            val intent = Intent(this@Valuations, Home::class.java)
             startActivity(intent)
         }
 
         val settings_button = findViewById<ImageView>(R.id.settings_settings) as ImageView
         settings_button.setOnClickListener {
             settings_button.setColorFilter(Color.RED)
-            val intent = Intent(this@valuationsActivity, Settings::class.java)
+            val intent = Intent(this@Valuations, Settings::class.java)
             startActivity(intent)
         }
 
         val calculadora_button = findViewById<ImageView>(R.id.calculadora_settings) as ImageView
         calculadora_button.setOnClickListener {
             calculadora_button.setColorFilter(Color.RED)
-            val intent = Intent(this@valuationsActivity, calculatorActivity::class.java)
+            val intent = Intent(this@Valuations, Calculator::class.java)
             startActivity(intent)
         }
 
         val save_button = findViewById<Button>(R.id.button3) as Button
         save_button.setOnClickListener {
-            val intent = Intent(this@valuationsActivity, Home::class.java)
+            val intent = Intent(this@Valuations, Home::class.java)
             startActivity(intent)
         }
 
         val cancel_button = findViewById<Button>(R.id.button2) as Button
         cancel_button.setOnClickListener {
-            val intent = Intent(this@valuationsActivity, Home::class.java)
+            val intent = Intent(this@Valuations, Home::class.java)
             startActivity(intent)
         }
     }
