d09aa843dbc946798f16dafd459eec6f58d2d47d
ainagil <ainagiltecno@gmail.com>
2020-03-21 16:17:32 +0100

    funcionalitat botons

--- app/src/main/java/com/example/somnia/calculatorActivity.kt
+++ app/src/main/java/com/example/somnia/calculatorActivity.kt
@@ -1,12 +1,30 @@
 package com.example.somnia
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class calculatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        val home_button = findViewById<ImageView>(R.id.principal_settings) as ImageView
+        home_button.setOnClickListener {
+            home_button.setColorFilter(Color.RED)
+            val intent = Intent(this@calculatorActivity, Home::class.java)
+            startActivity(intent)
+        }
+
+        val settings_button = findViewById<ImageView>(R.id.settings_settings) as ImageView
+        settings_button.setOnClickListener {
+            settings_button.setColorFilter(Color.RED)
+            val intent = Intent(this@calculatorActivity, Settings::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/somnia/valuationsActivity.kt
+++ app/src/main/java/com/example/somnia/valuationsActivity.kt
@@ -1,12 +1,49 @@
 package com.example.somnia
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class valuationsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_valuations)
+
+        val home_button = findViewById<ImageView>(R.id.principal_settings) as ImageView
+        home_button.setOnClickListener {
+            home_button.setColorFilter(Color.RED)
+            val intent = Intent(this@valuationsActivity, Home::class.java)
+            startActivity(intent)
+        }
+
+        val settings_button = findViewById<ImageView>(R.id.settings_settings) as ImageView
+        settings_button.setOnClickListener {
+            settings_button.setColorFilter(Color.RED)
+            val intent = Intent(this@valuationsActivity, Settings::class.java)
+            startActivity(intent)
+        }
+
+        val calculadora_button = findViewById<ImageView>(R.id.calculadora_settings) as ImageView
+        calculadora_button.setOnClickListener {
+            calculadora_button.setColorFilter(Color.RED)
+            val intent = Intent(this@valuationsActivity, calculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        val save_button = findViewById<Button>(R.id.button3) as Button
+        save_button.setOnClickListener {
+            val intent = Intent(this@valuationsActivity, Home::class.java)
+            startActivity(intent)
+        }
+
+        val cancel_button = findViewById<Button>(R.id.button2) as Button
+        cancel_button.setOnClickListener {
+            val intent = Intent(this@valuationsActivity, Home::class.java)
+            startActivity(intent)
+        }
     }
 }
