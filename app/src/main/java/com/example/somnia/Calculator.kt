5dbe3cc4e371f0740f7ec53f826bd5729251e31c
Gonzalo <gonzalourciuoli@gmail.com>
2020-03-22 21:16:37 +0100

    1

--- app/src/main/java/com/example/somnia/Calculator.kt
+++ app/src/main/java/com/example/somnia/Calculator.kt
@@ -8,6 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 public class Calculator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.alarms)
+        setContentView(R.layout.activity_calculator)
     }
 }
