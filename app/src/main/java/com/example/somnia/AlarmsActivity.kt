bfa539bbed500d99689c9d87405400327f816fb6
Gonzalo <gonzalourciuoli@gmail.com>
2020-03-26 09:01:25 +0100

    1

--- app/src/main/java/com/example/somnia/AlarmsActivity.kt
+++ app/src/main/java/com/example/somnia/AlarmsActivity.kt
@@ -8,15 +8,16 @@ import android.widget.Button
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.alarms.*
 
-public class AlarmsActivity : AppCompatActivity() {
+class AlarmsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alarms)
-
-    val newAlarm_button = findViewById<Button>(R.id.add) as Button
+/*
+val newAlarm_button = findViewById<Button>(R.id.add) as Button
         newAlarm_button.setOnClickListener {
             val intent = Intent(this@AlarmsActivity, New_alarmActivity::class.java)
             startActivity(intent)
         }
+ */
     }
 }
