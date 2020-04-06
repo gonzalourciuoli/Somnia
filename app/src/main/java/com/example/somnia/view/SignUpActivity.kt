cab9737ae32ba15acfde4df565cf23dc7af379a7
gonzalourciuoli <gonzalourciuoli@gmail.com>
2020-04-06 19:51:10 +0200

    Log-in hecho

--- app/src/main/java/com/example/somnia/view/SignUpActivity.kt
+++ app/src/main/java/com/example/somnia/view/SignUpActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -20,8 +21,8 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var txtEmail : EditText
     private lateinit var txtPassword : EditText
     private lateinit var txtRepeat_password : EditText
-    private lateinit var database : FirebaseDatabase
     private lateinit var dbReference : DatabaseReference
+    private lateinit var database : FirebaseDatabase
     private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,25 +39,17 @@ class SignUpActivity : AppCompatActivity() {
 
         dbReference = database.reference.child("User")
 
-
-        val signup_button = findViewById<Button>(R.id.sign_up) as Button
-        signup_button.setOnClickListener {
-            val accountExit : Int = createNewAccount()
-            if (accountExit == 0) {
-                val intent = Intent(this@SignUpActivity, Home::class.java)
-                startActivity(intent)
-            }
-        }
-
-        val login_button = findViewById<Button>(R.id.log_in) as Button
-        login_button.setOnClickListener {
-            val intent = Intent(this@SignUpActivity, Home::class.java)
-            startActivity(intent)
-        }
-
     }
 
-    private fun createNewAccount() : Int {
+    fun sign_up(view : View) {
+        createNewAccount()
+    }
+
+    fun login(view : View) {
+        startActivity(Intent(this, logInActivity::class.java))
+    }
+
+    private fun createNewAccount() {
         val username : String = txtUsername.text.toString()
         val email : String = txtEmail.text.toString()
         val password : String = txtPassword.text.toString()
@@ -64,7 +57,6 @@ class SignUpActivity : AppCompatActivity() {
 
         if (password != repeat_password) {
             Toast.makeText(this, "The passwords aren't matching", Toast.LENGTH_LONG).show()
-            return 1
         } else if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(repeat_password)) {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
                 task ->
@@ -73,14 +65,13 @@ class SignUpActivity : AppCompatActivity() {
                     val user : FirebaseUser? = auth.currentUser
                     verifyEmail(user)
 
-                    val userBD = dbReference.child(user!!.uid)
+                    val userBD = dbReference.child(user?.uid.toString())
                     userBD.child("Name").setValue(username)
+                    startActivity(Intent(this, logInActivity::class.java))
                 }
             }
-            return 0
         } else {
             Toast.makeText(this, "Looks like some of the fields are empty", Toast.LENGTH_LONG).show()
-            return 2
         }
     }
 
--- app/src/main/java/com/example/somnia/view/logInActivity.kt
+++ app/src/main/java/com/example/somnia/view/logInActivity.kt
@@ -3,29 +3,60 @@ package com.example.somnia.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.somnia.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class logInActivity : AppCompatActivity() {
+
+    private lateinit var txtUsername : EditText
+    private lateinit var txtPassword : EditText
+
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val login_button = findViewById<Button>(R.id.login) as Button
-        login_button.setOnClickListener {
-            val intent = Intent(this@logInActivity, Home::class.java)
-            startActivity(intent)
+        txtUsername = findViewById(R.id.user)
+        txtPassword = findViewById(R.id.password)
+
+        auth = FirebaseAuth.getInstance()
+
+    }
+
+    fun login(view : View) {
+        loginUser()
+    }
+
+    fun sign_up(view : View) {
+        startActivity(Intent(this, SignUpActivity::class.java))
+    }
+
+    fun forgot_password(view : View) {
+
+    }
+
+    private fun loginUser() {
+        val username : String = txtUsername.text.toString()
+        val password : String = txtPassword.text.toString()
+
+        if(!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+            auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(this) {
+                task ->
+
+                if (task.isSuccessful) {
+                    startActivity(Intent(this, Home::class.java))
+                } else {
+                    Toast.makeText(this, "Error en la autenticación", Toast.LENGTH_LONG).show()
+                }
+            }
         }
-
-        val signup_button = findViewById<Button>(R.id.sign_up) as Button
-        signup_button.setOnClickListener {
-            val intent = Intent(this@logInActivity, SignUpActivity::class.java)
-            startActivity(intent)
-
-        }
-
-
     }
 
 }
