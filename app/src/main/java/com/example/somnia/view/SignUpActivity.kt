08e4832fc64e16c9a27493e47de3c412a7192ed4
gonzalourciuoli <gonzalourciuoli@gmail.com>
2020-04-09 21:06:59 +0200

    Log-in hecho

--- app/src/main/java/com/example/somnia/view/SignUpActivity.kt
+++ app/src/main/java/com/example/somnia/view/SignUpActivity.kt
@@ -1,9 +1,11 @@
 package com.example.somnia.view
 
 import android.content.Intent
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -13,17 +15,17 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.signup.*
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var txtUsername : EditText
-    private lateinit var txtEmail : EditText
-    private lateinit var txtPassword : EditText
-    private lateinit var txtRepeat_password : EditText
-    private lateinit var dbReference : DatabaseReference
-    private lateinit var database : FirebaseDatabase
-    private lateinit var auth : FirebaseAuth
+    private lateinit var txtUsername: EditText
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var txtRepeat_password: EditText
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,28 +36,42 @@ class SignUpActivity : AppCompatActivity() {
         txtPassword = findViewById(R.id.password)
         txtRepeat_password = findViewById(R.id.repeat_password)
 
-        database = FirebaseDatabase.getInstance()
+        db = FirebaseFirestore.getInstance()
         auth = FirebaseAuth.getInstance()
 
-        dbReference = database.reference.child("User")
+        val user = hashMapOf(
+            "username" to "User Prueba",
+            "email" to "Email Prueba",
+            "password" to "Password prueba"
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Toast.makeText(this, "User created", Toast.LENGTH_LONG)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "User creation failed", Toast.LENGTH_LONG)
+            }
+
 
     }
 
-    fun sign_up(view : View) {
+    fun sign_up(view: View) {
         createNewAccount()
     }
 
-    fun login(view : View) {
+    fun login(view: View) {
         startActivity(Intent(this, logInActivity::class.java))
     }
 
     private fun createNewAccount() {
-        val username : String = txtUsername.text.toString()
-        val email : String = txtEmail.text.toString()
-        val password : String = txtPassword.text.toString()
-        val repeat_password : String = txtRepeat_password.text.toString()
+        val username: String = txtUsername.text.toString()
+        val email: String = txtEmail.text.toString()
+        val password: String = txtPassword.text.toString()
+        val repeat_password: String = txtRepeat_password.text.toString()
 
-        if (password != repeat_password) {
+        /*if (password != repeat_password) {
             Toast.makeText(this, "The passwords aren't matching", Toast.LENGTH_LONG).show()
         } else if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(repeat_password)) {
             auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
@@ -72,19 +88,22 @@ class SignUpActivity : AppCompatActivity() {
             }
         } else {
             Toast.makeText(this, "Looks like some of the fields are empty", Toast.LENGTH_LONG).show()
-        }
+        }*/
     }
 
-    private fun verifyEmail(user : FirebaseUser?) {
-         user?.sendEmailVerification()?.addOnCompleteListener(this) {
-             task ->
+    private fun verifyEmail(user: FirebaseUser?) {
+        user?.sendEmailVerification()?.addOnCompleteListener(this) { task ->
 
-             if (task.isComplete) {
-                 Toast.makeText(this, "Email sent", Toast.LENGTH_LONG).show()
-             } else {
-                 Toast.makeText(this, "An error occurred trying to send the email", Toast.LENGTH_LONG).show()
+            if (task.isComplete) {
+                Toast.makeText(this, "Email sent", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "An error occurred trying to send the email",
+                    Toast.LENGTH_LONG
+                ).show()
 
-             }
-         }
+            }
+        }
     }
 }
