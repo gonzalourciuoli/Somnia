9d25a197536b631de95ccae5d301a3fbfa2b7d49
Estibaliz26 <cangreja26>
2020-04-23 23:28:38 +0200

    alertdialog logout y delete account acabados

--- app/src/main/java/com/example/somnia/view/New_alarmActivity.kt
+++ app/src/main/java/com/example/somnia/view/New_alarmActivity.kt
@@ -1,18 +1,15 @@
 package com.example.somnia.view
 import com.example.somnia.R
-import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import kotlinx.android.synthetic.main.alarm_design.*
 
 
 class New_alarmActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.new_alarm)
+        setContentView(R.layout.activity_new_alarm)
 
         val monday_switch = findViewById<Switch>(R.id.monday_switch)
         val tuesday_switch = findViewById<Switch>(R.id.tuesday_switch)
