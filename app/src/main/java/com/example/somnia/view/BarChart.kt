2c35ca119f095b81e6e1adc889a84ff06cefae97
Estibaliz26 <cangreja26>
2020-04-26 07:14:32 +0200

    Graficas acabadas

--- app/src/main/java/com/example/somnia/view/BarChart.kt
+++ app/src/main/java/com/example/somnia/view/BarChart.kt
@@ -9,6 +9,9 @@ import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.anychart.graphics.vector.GradientKey
 import com.example.somnia.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlin.random.Random
 
 class BarChart : AppCompatActivity(){
@@ -16,15 +19,17 @@ class BarChart : AppCompatActivity(){
     val days = ArrayList<Int>()
     val hours1 = ArrayList<Int>()
 
+    private lateinit var auth : FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private lateinit var dataCollection : CollectionReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_chart)
 
-        for (i in 1..31) {
-            val value = Random.nextFloat() * 24
-            hours1.add(value.toInt())
-            days.add(i)
-        }
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        dataCollection = db.collection("bar_chart")
 
         val barChart: AnyChartView = findViewById(R.id.bar_chart)
         APIlib.getInstance().setActiveAnyChartView(barChart)
@@ -41,19 +46,30 @@ class BarChart : AppCompatActivity(){
 
         bar.animation(true)
 
-        for (i in days.indices){
-            entries.add(ValueDataEntry(days[i],hours1[i]))
-        }
+        dataCollection
+            .get()
+            .addOnSuccessListener {
 
-        bar.data(entries)
-        bar.title("MONTHLY")
-        bar.yAxis(0).title("Hours")
-        bar.xAxis(0).title("Days")
-        val rangeColors = arrayOf("#663399")
-        bar.palette(rangeColors)
-        background.stroke("#663399")
-        //background.fill(GradientKey("#dcd0ff",0,1))
-        barChart.setChart(bar)
+                for (document in it) {
+                    days.add(Integer.parseInt(document.get("xValue").toString()!!))
+                    hours1.add(Integer.parseInt(document.get("yValue").toString()!!))
+                }
+
+                for (i in days.indices) {
+                    entries.add(ValueDataEntry(days[i], hours1[i]))
+                }
+
+                bar.data(entries)
+                bar.title("MONTHLY")
+                bar.yAxis(0).title("Hours of sleep")
+                bar.xAxis(0).title("Days")
+                val rangeColors = arrayOf("#663399")
+                bar.palette(rangeColors)
+                background.stroke("#663399")
+                //background.fill(GradientKey("#dcd0ff",0,1))
+                barChart.setChart(bar)
+
+            }
 
     }
 }
