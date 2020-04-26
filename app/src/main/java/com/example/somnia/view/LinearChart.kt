58cb4f1536e99d577f8203d635b292ae8e39c727
gonzalourciuoli <gonzalourciuoli@gmail.com>
2020-04-26 03:15:07 +0200

    Update email

--- app/src/main/java/com/example/somnia/view/LinearChart.kt
+++ app/src/main/java/com/example/somnia/view/LinearChart.kt
@@ -15,6 +15,8 @@ import com.anychart.enums.MarkerType
 import com.anychart.graphics.vector.GradientKey
 import com.example.somnia.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlin.random.Random
 
 class LinearChart: AppCompatActivity()  {
@@ -23,16 +25,16 @@ class LinearChart: AppCompatActivity()  {
     val hours4 = ArrayList<Int>()
 
     private lateinit var auth : FirebaseAuth
+    private lateinit var db : FirebaseFirestore
+    private lateinit var layoutCollection : CollectionReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linear_chart)
 
-        for(i in 18..90){
-            ages.add(i)
-        }
-
         auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        layoutCollection = db.collection("linear_chart")
 
         val lineChart : AnyChartView = findViewById(R.id.linear_chart)
         APIlib.getInstance().setActiveAnyChartView(lineChart)
@@ -48,34 +50,42 @@ class LinearChart: AppCompatActivity()  {
 
         line.animation(true)
 
-        for (i in ages.indices){
-            val value = Random.nextFloat()* 24
-            hours4.add(value.toInt())
-            entries.add(ValueDataEntry(ages[i],hours4[i]))
+        layoutCollection.get().addOnSuccessListener {
+            for (doc in it) {
+                ages.add(Integer.parseInt(doc.get("xValue").toString()))
+                hours4.add(Integer.parseInt(doc.get("yValue").toString()))
+            }
+
+            for (i in ages.indices) {
+                //val value = Random.nextFloat()* 24
+                //hours4.add(value.toInt())
+                entries.add(ValueDataEntry(ages[i], hours4[i]))
+            }
+
+            val set = Set.instantiate()
+            set.data(entries) //aqui deberia pasarse los datos del firebase
+
+            val lineMap: Mapping = set.mapAs("{ x: 'x', value: 'value' }")
+
+            val l: Line = line.line(lineMap)
+            l.name("People")
+            l.hovered().markers().enabled(true)
+            l.hovered().markers().type(MarkerType.CIRCLE).size(4.0)
+            l.tooltip().position("right").anchor(Anchor.LEFT_CENTER).offsetX(5.0).offsetY(5.0)
+
+
+            line.title("AVERAGE OF POPULATION")
+            line.yAxis(0).title("Hours")
+            line.xAxis(0).title("Age")
+            line.legend(true)
+
+            val range_colors = arrayOf("#9933FF")
+            line.palette(range_colors)
+            background.stroke("#663399")
+            //background.fill(GradientKey("#dcd0ff",0,1))
+            lineChart.setChart(line)
         }
 
-        val set = Set.instantiate()
-        set.data(entries) //aqui deberia pasarse los datos del firebase
-
-        val lineMap: Mapping = set.mapAs("{ x: 'x', value: 'value' }")
-
-        val l: Line = line.line(lineMap)
-        l.name("People")
-        l.hovered().markers().enabled(true)
-        l.hovered().markers().type(MarkerType.CIRCLE).size(4.0)
-        l.tooltip().position("right").anchor(Anchor.LEFT_CENTER).offsetX(5.0).offsetY(5.0)
-
-
-        line.title("AVERAGE OF POPULATION")
-        line.yAxis(0).title("Hours")
-        line.xAxis(0).title("Age")
-        line.legend(true)
-
-        val range_colors = arrayOf("#9933FF")
-        line.palette(range_colors)
-        background.stroke("#663399")
-        //background.fill(GradientKey("#dcd0ff",0,1))
-        lineChart.setChart(line)
     }
 
 }
