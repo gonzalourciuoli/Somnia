ec1adf24f644a10adebfd57f82808c91bb2c5071
gonzalourciuoli <gonzalourciuoli@gmail.com>
2020-04-26 22:13:52 +0200

    Update email

--- app/src/main/java/com/example/somnia/view/Buttons_ListCalendar.kt
+++ app/src/main/java/com/example/somnia/view/Buttons_ListCalendar.kt
@@ -5,13 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.somnia.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Buttons_ListCalendar : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buttons__list_calendar)
-
         val calendar = findViewById<Button>(R.id.calendar) as Button
         calendar.setOnClickListener {
             val intent = Intent(this, Calendar::class.java)
@@ -22,6 +23,7 @@ class Buttons_ListCalendar : AppCompatActivity() {
         list.setOnClickListener {
             val intent = Intent(this, InfoValuations::class.java)
             startActivity(intent)
+
         }
 
         val returnButton = findViewById<Button>(R.id.return_button) as Button
@@ -30,4 +32,5 @@ class Buttons_ListCalendar : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
 }
